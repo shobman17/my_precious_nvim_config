a649ec4d7b8df196d067725a8ff3a3521e484a5a
shob1707 <210070081@iitb.ac.in>
2024-08-17 17:40:24 +0530

    Initial config

---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,16 @@
+-- Change Theme here
+-- require("core.plugin_config.gruvbox")
+require("core.plugin_config.nightfly")
+
+-- Change version of lualine here
+-- require("core.plugin_config.lualine")
+require("core.plugin_config.evil_lualine")
+require("core.plugin_config.bufferline")
+
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.treesitter")
+require("core.plugin_config.telescope")
+
+-- Change LSP configs here
+require("core.plugin_config.lsp_config")
+
---
+++ lua/core/plugins.lua
@@ -0,0 +1,53 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- themes!
+  use 'ellisonleao/gruvbox.nvim'
+  use 'bluz71/vim-nightfly-colors'
+  use 'shaunsingh/nord.nvim'
+
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-lualine/lualine.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    requires = {{'nvim-lua/plenary.nvim'}}
+  }
+
+  use {
+    'akinsho/bufferline.nvim', 
+    tag = "*", requires = 'nvim-tree/nvim-web-devicons'
+  }
+  -- lsp functionality
+  use {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+  }
+
+  -- telescope helper packages
+  use { 
+     'nvim-telescope/telescope-fzf-native.nvim', 
+     run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' 
+  }
+
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
